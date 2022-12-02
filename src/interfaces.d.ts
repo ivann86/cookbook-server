c271da06eba419a4fafc4fd67998b4c3a9cb7938
Ivan Ivanov <ivan.m.ivanov@icloud.com>
2022-12-02 14:47:19 +0200

    getById method

--- src/interfaces.d.ts
+++ src/interfaces.d.ts
@@ -1,7 +1,8 @@
 interface DataStore<T> {
-  create: (user: T) => Promise<T>;
+  create: (object: T) => Promise<T>;
+  getById: (id: string, options: any) => Promise<T | null>;
   getAll: (options: any) => Promise<T[]>;
   updateOne: (filter: any, update: T) => Promise<T>;
-  updateMany: (filter: any, update: T) => Promise<T>;
+  updateMany: (filter: any, update: T) => Promise<Array<T>>;
   remove: (id: string) => Promise<Boolean>;
 }
